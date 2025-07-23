87df1fe2a018ceee2c2da17d5d6fcc39b785fb97
Arshveer Nagra <arshveer.nagra@mail.utoronto.ca>
2025-07-22 22:19:14 -0400

    starting files

---
+++ src/ProjectFiles/StudyApp.java
@@ -0,0 +1,17 @@
+package ProjectFiles;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * This is the main entr poiht to the appilcation.
+ * This class initialzed the MVC architecture
+ */
+
+public class StudyApp extends Application {
+    View view;
+
+    public static void main(String[] args) {    launch(args)    }
+
+
+}
---
+++ src/ProjectFiles/View.java
@@ -0,0 +1,25 @@
+package ProjectFiles;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+/**
+ * This class handles the visual for the user with MVC architecture in mind
+ */
+public class View implements EventHandler<ActionEvent> {
+    private stage;
+
+    public View(Stage stage) {
+        this.stage = stage;
+        initUI(stage);
+    }
+}
